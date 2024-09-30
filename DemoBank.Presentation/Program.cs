2f7e5f0abc4e0cad1e2ede8bcc9d8a9763a9dcc7
shoaibalirts <shoaibali.rts@gmail.com>
2024-09-30 17:34:26 +0200

    Add project files.

---
+++ DemoBank.Presentation/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //display title
+        System.Console.WriteLine("*********** Demo Bank *************");
+        System.Console.WriteLine("::Login Page::");
+
+        string userName = null, password = null;
+        System.Console.Write("Username:");
+        userName = System.Console.ReadLine();
+        if (userName != "")
+        {
+            System.Console.Write("Password:");
+            password = System.Console.ReadLine();
+        }
+        if(userName == "system" && password == "manager")      
+        {
+            System.Console.WriteLine("TO DO: Main menu here");
+        }
+        System.Console.WriteLine("Thank you! Visit again");
+        System.Console.ReadKey();
+    }
+}
